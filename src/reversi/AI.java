6c7db4b5db64d6dc8f84118479f2eeb8ad6a5c39
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2013-04-26 12:51:14 -0700

    delete duplicate of getMove method

--- src/reversi/AI.java
+++ src/reversi/AI.java
@@ -121,13 +121,5 @@ public class AI {
 		//return move;
 		
 		return null;
-	}public Coordinate<Integer,Integer> getMove(Tile.State player) {
-		
-		//Search for next best move for player
-		
-		//Coordinate move = new Coordinate<Integer,Integer>(1,2);
-		//return move;
-		
-		return null;
 	}
 }
