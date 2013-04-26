088fa53b1228668756942b7838ad6c2fff992b54
Patrick Mosca <mosca1337@pacbell.net>
2013-04-25 22:11:31 -0700

    Added AI Object

---
+++ src/reversi/AI.java
@@ -0,0 +1,14 @@
+package reversi;
+
+public class AI {
+
+	public Coordinate<Integer,Integer> getMove(Tile.State player) {
+		
+		//Search for next best move for player
+		
+		//Coordinate move = new Coordinate<Integer,Integer>(1,2);
+		//return move;
+		
+		return null;
+	}
+}
